d7c46ee449a7e7353c1a5f0d2e92757a855b88c0
farukalways <alishopon367@gmail.com>
2025-11-26 21:10:03 +0600

    create login form

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -4,10 +4,14 @@ const Field = ({ label, children, htmlFor, error }) => {
   const id = htmlFor || getChildId(children);
 
   return (
-    <div>
-      {label && <label htmlFor={id}>{label}</label>}
+    <div className="flex flex-col items-start justify-start mt-2 p-0 w-full mr-2">
+      {label && (
+        <label htmlFor={id} className="mb-1">
+          {label}
+        </label>
+      )}
       {children}
-      {!!error && { error }}
+      {!!error && <div className="text-red-500">{error.message}</div>}
     </div>
   );
 };
--- src/forms/LoginForm.jsx
+++ src/forms/LoginForm.jsx
@@ -1,8 +1,60 @@
+import { useForm } from "react-hook-form";
+import Field from "../components/Field";
+import FieldSet from "../components/FieldSet";
+
 const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitForm = (formData) => {
+    console.log(formData);
+  };
+
   return (
-    <div>
-      <h2>Login</h2>
-      <h2>Login</h2>
+    <div className="flex flex-col justify-center items-center">
+      <form onSubmit={handleSubmit(submitForm)}>
+        <FieldSet label="Enter Login Details">
+          <Field label="Email" error={errors.email}>
+            <input
+              {...register("email", { required: "Email is required." })}
+              className={`p-2 border box-border w-[300px] rounded-md ${
+                errors.email ? "border-red-500" : "border-gray-200"
+              }`}
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter email address"
+            />
+          </Field>
+          <Field label="Password" error={errors.password}>
+            <input
+              {...register("password", {
+                required: "Password is required.",
+                minLength: {
+                  value: 8,
+                  message: "Your password must be at least 8 characters.",
+                },
+              })}
+              className={`p-2 border box-border w-[300px] rounded-md ${
+                errors.password ? "border-red-500" : "border-gray-200"
+              }`}
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter Password"
+            />
+          </Field>
+        </FieldSet>
+        <div>{errors?.root?.random?.message}</div>
+        <Field>
+          <button className="text-md text-white cursor-pointer p-1 border rounded-lg bg-purple-500 m-auto">
+            Login
+          </button>
+        </Field>
+      </form>
     </div>
   );
 };
